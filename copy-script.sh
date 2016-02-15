d8a3ae6b4d10b1853e99258ff97b489d574057f8
mikaelec <mikaelec@gmail.com>
2016-02-15 18:56:03 +0100

    Added arg for choosing to copy either in or out.

--- copy-script.sh
+++ copy-script.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
 var exec = require('child_process').exec;
+var arg = process.argv[2];
+var moveFunc;
+switch (arg) {
+  case 'out':
+    moveFunc = moveFile;
+    break;
+  case 'in':
+    moveFunc = moveFileReverse;
+    break;
+  default:
+    console.log(`Bad argument: '${arg}', accepts only arg of value 'out' or 'in'.`);
+    return;
+}
 
 const FILE_MAP = {
   '.bashrc': '~/.bashrc',
@@ -13,11 +26,11 @@ const FILE_MAP = {
   'snippets.cson': '~/.atom/snippets.cson'
 };
 
-var fetchPromised = (src, dest) => new Promise((resolve, reject) => { moveFileAsync(src, dest, resolve, reject); });
+var fetchPromised = (src, dest) => new Promise((resolve, reject) => { moveFunc(src, dest, resolve, reject); });
 
 Promise.all(Object.keys(FILE_MAP).map(x => fetchPromised(x, FILE_MAP[x])));
 
-function moveFileAsync(src, dest, onSuccess, onError) {
+function moveFile(src, dest, onSuccess, onError) {
   exec(`cp ${src} ${dest}`, (error, stdout, stderr) => {
     if (error) {
       console.log(error.message.match(/cp\:.*/g)[0]);
@@ -30,4 +43,8 @@ function moveFileAsync(src, dest, onSuccess, onError) {
     }
   });
 
+}
+
+function moveFileReverse(src, dest, onSuccess, onError) {
+  moveFile(dest, src, onSuccess, onError);
 }
