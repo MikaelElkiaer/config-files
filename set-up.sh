89408644fc71e75b584e7a03ef6ab7d64614df6f
Mikael Elkiær <2102306+MikaelElkiaer@users.noreply.github.com>
2019-07-03 11:23:48 +0200

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# get scripts
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/git-prompt.sh
+
 # set up symbolic links
 ln -sf ./.gitconfig ~/.gitconfig
 ln -sf ./.vimrc ~/.vimrc
